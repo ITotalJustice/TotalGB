2af3f0a52d8c2885e90de6cdbf266e4205d79a6a
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2021-03-23 15:30:09 +0000

    remove discord-rpc and rapidjson
    rapidjson was needed for discord only, hence the removal.
    
    decided that im probably not going to add discord support for a *long* time, if at all, so might as well not keep it in the codebase

--- src/frontend/mgb.cpp
+++ src/frontend/mgb.cpp
@@ -6,80 +6,10 @@
 #include "util/log.hpp"
 
 #include "nativefiledialog/nfd.hpp"
-#ifdef USE_DISCORD
-#include <discord_rpc/discord_rpc.h>
-#endif // USE_DISCORD
 
 #include <cstring>
 #include <cassert>
 #include <fstream>
-
-#ifdef USE_DISCORD
-static const char* APPLICATION_ID = "822897241202884618";
-
-static void UpdatePresence() {
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Debug";
-    discordPresence.details = "Coding away...";
-    discordPresence.startTimestamp = 1507665886;
-    discordPresence.endTimestamp = 1507665886;
-    discordPresence.largeImageKey = "test";
-    discordPresence.largeImageText = "TotalGB";
-    discordPresence.smallImageKey = "test";
-    discordPresence.smallImageText = "Rogue - Level 100";
-    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-    discordPresence.partySize = 0;
-    discordPresence.partyMax = 5;
-    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-    Discord_UpdatePresence(&discordPresence);
-}
-
-static void handleDiscordReady(const DiscordUser* connectedUser) {
-    printf("\nDiscord: connected to user %s#%s - %s\n",
-           connectedUser->username,
-           connectedUser->discriminator,
-           connectedUser->userId);
-}
-
-static void handleDiscordDisconnected(int errcode, const char* message) {
-    printf("\nDiscord: disconnected (%d: %s)\n", errcode, message);
-}
-
-static void handleDiscordError(int errcode, const char* message) {
-    printf("\nDiscord: error (%d: %s)\n", errcode, message);
-}
-
-static void handleDiscordJoin(const char* secret) {
-    printf("\nDiscord: join (%s)\n", secret);
-}
-
-static void handleDiscordSpectate(const char* secret) {
-    printf("\nDiscord: spectate (%s)\n", secret);
-}
-
-static void handleDiscordJoinRequest(const DiscordUser* request) {
-    int response = -1;
-    // response = DISCORD_REPLY_NO;
-    response = DISCORD_REPLY_YES;
-
-    if (response != -1) {
-        Discord_Respond(request->userId, response);
-    }
-}
-
-static void discordInit() {
-    DiscordEventHandlers handlers;
-    memset(&handlers, 0, sizeof(handlers));
-    handlers.ready = handleDiscordReady;
-    handlers.disconnected = handleDiscordDisconnected;
-    handlers.errored = handleDiscordError;
-    handlers.joinGame = handleDiscordJoin;
-    handlers.spectateGame = handleDiscordSpectate;
-    handlers.joinRequest = handleDiscordJoinRequest;
-    Discord_Initialize(APPLICATION_ID, &handlers, 1, NULL);
-}
-#endif // USE_DISCORD
 
 namespace mgb {
 
@@ -332,10 +262,6 @@ auto Instance::GetGB() -> GB_Data* {
 }
 
 App::App() {
-#ifdef USE_DISCORD
-    discordInit();
-#endif
-
     {
         SDL_version compiled;
         SDL_version linked;
@@ -370,10 +296,6 @@ App::~App() {
     }
 
 	SDL_Quit();
-
-#ifdef USE_DISCORD
-    Discord_Shutdown();
-#endif
 }
 
 auto App::LoadRom(const std::string& path, bool dual) -> bool {
@@ -491,14 +413,6 @@ auto App::Loop() -> void {
 
         // render the screen
         this->Draw();
-
-    #ifdef USE_DISCORD
-        UpdatePresence();
-    #ifdef DISCORD_DISABLE_IO_THREAD
-        Discord_UpdateConnection();
-    #endif // DISCORD_DISABLE_IO_THREAD
-        Discord_RunCallbacks();
-    #endif // USE_DISCORD
     }
 }
 
